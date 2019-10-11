ec86eb7a311f3c2af544cde00190539d730c1861
john <john@spinningplates.org>
2019-10-10 18:45:40 -0700

    added some styling

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,53 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withFormik, Form, Field } from 'formik';
 import axios from 'axios';
 import * as Yup from 'yup';
 
-const UserForm = ({ values, errors, touched }) => {
-  console.log(values.tos);
-  console.log(touched.fname);
+const UserForm = ({ values, errors, touched, status }) => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    console.log(status);
+    if (status) {
+      setUsers([...users, status]);
+    }
+  }, [users, status]);
+
   return (
-    <div>
+    <div className="user-form">
       <Form>
-        <div>
-          <Field type="text" name="fname" placeholder="Full Name" />
-          {touched.fname && errors.fname && (
-            <p className="error">{errors.fname}</p>
-          )}
-        </div>
-        <div>
-          <Field type="email" name="email" placeholder="Email" />
-          {touched.email && errors.email && (
-            <p className="error">{errors.email}</p>
-          )}
-        </div>
-        <div>
-          <Field type="password" name="password" placeholder="Password" />
-          {touched.password && errors.password && (
-            <p className="error">{errors.password}</p>
-          )}
-        </div>
-        <div>
+        <Field type="text" name="fname" placeholder="Full Name" />
+        {touched.fname && errors.fname && (
+          <p className="error">{errors.fname}</p>
+        )}
+
+        <Field type="email" name="email" placeholder="Email" />
+        {touched.email && errors.email && (
+          <p className="error">{errors.email}</p>
+        )}
+
+        <Field type="password" name="password" placeholder="Password" />
+        {touched.password && errors.password && (
+          <p className="error">{errors.password}</p>
+        )}
+
+        <Field component="select" name="role" className="role-select">
+          <option value="">Role is required</option>
+          <option value="UX">UX</option>
+          <option value="Frontend">Frontend</option>
+          <option value="Backend">Backend</option>
+          <option value="Full Stack">Full Stack</option>
+        </Field>
+        {touched.role && errors.role && <p className="error">{errors.role}</p>}
+
+        <div className="tos">
           <label>
             <Field type="checkbox" name="tos" checked={values.tos} />
-            Accept Terms of Service
+            <span>Accept Terms of Service</span>
           </label>
           {touched.tos && errors.tos && <p className="error">{errors.tos}</p>}
         </div>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
+
+        <button type="submit">Submit</button>
       </Form>
+      <h2>User Lists</h2>
+      {users.map(user => (
+        <ul key={user.id}>
+          <li>Id: {user.id}</li>
+          <li>User: {user.fname}</li>
+          <li>Email: {user.email}</li>
+          <li>Role: {user.role}</li>
+        </ul>
+      ))}
     </div>
   );
 };
 
 const FormikUserForm = withFormik({
-  mapPropsToValues({ fname, email, password, tos }) {
+  mapPropsToValues({ fname, email, password, role, tos }) {
     return {
       fname: fname || '',
       email: email || '',
       password: password || '',
+      role: role || '',
       tos: tos || false
     };
   },
@@ -60,18 +82,19 @@ const FormikUserForm = withFormik({
     password: Yup.string()
       .min(6, 'Password must be 6 characters or longer')
       .required('Password is required'),
+    role: Yup.string().required('Please choose a role'),
     tos: Yup.boolean().oneOf([true], 'Must Accept TOS')
   }),
 
-  handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
-    console.log(values);
-    if (values.email === 'john@email.com') {
+  handleSubmit(values, { resetForm, setErrors, setSubmitting, setStatus }) {
+    if (values.email === 'waffle@syrup.com') {
       setErrors({ email: 'That email is already taken' });
     } else {
       axios
         .post('https://reqres.in/api/users', values)
         .then(res => {
           console.log(res.data);
+          setStatus(res.data);
           resetForm();
           setSubmitting(false);
         })
